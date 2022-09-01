ac40076e64016a2cc4ab4ce7364637124aed2403
Rebecca Katarina Munck Schütze <rebe7917@stud.kea.dk>
2022-09-01 08:52:14 +0200

    annas metode

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 "use strict";
 
-/* let counter = 0;
-const array = [];
-console.log(array);
 
-start();
-
-function start(){
-array.unshift(counter);
-console.log(array);
-counter++;
-setTimeout(start, 500);
-} */
 
 const arr = [];
 let counter = 0;
 init();
 
 function init(){
+    // cpunter skal plusses med en 
+    // uden ville den skifte tallet ud og ikke tilføje
 counter += 1;
+// putter tallet foran plads 0
 arr.unshift(counter);
 console.log(arr.slice(0,9));
+//genstarter init functionen 
 setTimeout(init, 500);
 }
 
+
+/* let counter = 0;
+const arr = [];
+console.log(arr);
+
+start();
+
+function start() {
+  arr.unshift(counter);
+  console.log(arr);
+  counter++;
+  if (arr.length > 8) {
+    arr.pop();
+  }
+  setTimeout(start, 1000);
+  console.log(counter);
+} */
